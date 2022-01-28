21a2dcc43d0bf751cbadaa377ba185e7d3e39d9c
René Modia <renelog.modia@gamail.com>
2022-01-28 17:01:54 +0100

    add html header

---
+++ resources/views/layouts/partials/header.blade.php
@@ -0,0 +1,36 @@
+<div class="nav-h">
+    <div>
+        <img src='{{Storage::url("assets/logo_EGJC.jpg")}}' alt="Logo EGJC">
+    </div>
+
+    <div class="nav-h_item-container">
+        <ul>
+                <li class="nav-h_item-container_item"><a href="#">Acueil</a></li>
+                <li class="nav-h_item-container_item"><a href="#">A propos de nous</a></li>
+                <li><a href="#">Activités</a></li>
+                <li><a href="#">Actualités</a></li>
+                <li><a href="#">Département</a></li>
+                <li><a href="#">Evènement</a></li>
+                <li><a href="#">Gallérie</a></li>
+                <li><a href="#">Faire un don</a></li>
+                <li><a href="#">Nous contacter</a></li>
+                <li><a href="#">Nous contacter</a></li>
+                <li><a href="#">Gestion</a></li>
+            </ul>
+    </div>
+</div>
+
+<div class="banner">
+    <div>
+        <h1>EGLISE GLORIEUSE DE JESUS-CHRIST</h1>
+        <h5>Evangile, Crainte de Dieu, Foi, Amour , Salut</h5>
+        <img src='{{Storage::url("assets/Bible-PNG-Isolated-Transparent.png")}}' alt="Image bible">
+        <p>Juges 6:25-26</p>
+    </div>
+    <div class="container-purple-header-img">
+        <img src='{{Storage::url("assets/Trapeze_violet_header.webp")}}'  class="purple-headerimg" alt="Imge de fond de l'en-tête de la page">
+    </div>
+    <div class="container-photo-bg-img">
+        <img src="public\storage\assets\img_head_bg.webp" class="purple-headerimg" alt="Imge de fond de l'en-tête de la page">
+    </div>
+</div>
--- webpack.mix.js
+++ webpack.mix.js
@@ -16,3 +16,4 @@ mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'pub
     require('tailwindcss'),
     require('autoprefixer'),
 ]);
+mix.sass('resources/scss/app.scss', 'public/css');
