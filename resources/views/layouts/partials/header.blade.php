51f19cb1a33531511054c48ade1e4396329ccd2f
René Modia <renelog.modia@gamail.com>
2022-01-29 02:29:36 +0100

    save

--- resources/views/layouts/partials/header.blade.php
+++ resources/views/layouts/partials/header.blade.php
@@ -1,36 +1,36 @@
 <div class="nav-h">
-    <div>
-        <img src='{{Storage::url("assets/logo_EGJC.jpg")}}' alt="Logo EGJC">
+    <div class = "nav-h_logoContainer">
+        <img src = '{{ Storage::url("assets/logo_EGJC.jpg") }}'  class = "nav-h_logoContainer_logo" alt="Logo EGJC">
     </div>
 
     <div class="nav-h_item-container">
-        <ul>
                 <li class="nav-h_item-container_item"><a href="#">Acueil</a></li>
                 <li class="nav-h_item-container_item"><a href="#">A propos de nous</a></li>
-                <li><a href="#">Activités</a></li>
-                <li><a href="#">Actualités</a></li>
-                <li><a href="#">Département</a></li>
-                <li><a href="#">Evènement</a></li>
-                <li><a href="#">Gallérie</a></li>
-                <li><a href="#">Faire un don</a></li>
-                <li><a href="#">Nous contacter</a></li>
-                <li><a href="#">Nous contacter</a></li>
-                <li><a href="#">Gestion</a></li>
-            </ul>
+                <li class="nav-h_item-container_item"><a href="#">Activités</a></li>
+                <li class="nav-h_item-container_item"><a href="#">Actualités</a></li>
+                <li class="nav-h_item-container_item"><a href="#">Département</a></li>
+                <li class="nav-h_item-container_item"><a href="#">Evènement</a></li>
+                <li class="nav-h_item-container_item"><a href="#">Gallérie</a></li>
+                <li class="nav-h_item-container_item"><a href="#">Faire un don</a></li>
+                <li class="nav-h_item-container_item"><a href="#">Nous contacter</a></li>
+                <li class="nav-h_item-container_item"><a href="#">Nous contacter</a></li>
+                <li class="nav-h_item-container_item"><a href="#">Gestion</a></li>
     </div>
+
 </div>
 
-<div class="banner">
-    <div>
-        <h1>EGLISE GLORIEUSE DE JESUS-CHRIST</h1>
-        <h5>Evangile, Crainte de Dieu, Foi, Amour , Salut</h5>
-        <img src='{{Storage::url("assets/Bible-PNG-Isolated-Transparent.png")}}' alt="Image bible">
-        <p>Juges 6:25-26</p>
+<div class="bannerHeader">
+    <div class = "bannerHeader_containerTitle">
+        <h1 class = "bannerHeader_containerTitle_h1">EGLISE GLORIEUSE</h1>
+        <h1 class = "bannerHeader_containerTitle_h1">DE JESUS-CHRIST</h1>
+        <h5 class = "bannerHeader_containerTitle_h5">Evangile,  Crainte de Dieu,  Foi,  Amour ,  Salut</h5>
+        <img class = "bannerHeader_containerTitle_bibleImg" src='{{Storage::url("assets/Bible-PNG-Isolated-Transparent.png")}}' alt="Image bible">
+        <p class = "bannerHeader_containerTitle_verse">Juges 6:25-26</p>
     </div>
-    <div class="container-purple-header-img">
-        <img src='{{Storage::url("assets/Trapeze_violet_header.webp")}}'  class="purple-headerimg" alt="Imge de fond de l'en-tête de la page">
+    <div class="bannerHeader_container-Trapeze">
+        <img class="bannerHeader_container-Trapeze_purpleTrapeze" src='{{Storage::url("assets/Trapeze_violet_header.webp")}}'  class="purple-headerimg" alt="Imge de fond de l'en-tête de la page">
     </div>
-    <div class="container-photo-bg-img">
-        <img src="public\storage\assets\img_head_bg.webp" class="purple-headerimg" alt="Imge de fond de l'en-tête de la page">
+    <div class="bannerHeader_container-bgImg">
+        <img class="bannerHeader_container-bgImg_photo" src='{{Storage::url("assets/img_header1.jpg")}}'  class="purple-headerimg" alt="Imge de fond de l'en-tête de la page">
     </div>
 </div>
